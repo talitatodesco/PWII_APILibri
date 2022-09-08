299e33fb8b1ffc53893b939989891199773f9b03
Talita Marcondes <70537487+talitatodesco@users.noreply.github.com>
2022-09-08 20:03:35 -0300

    Aula presencial 08_09

--- route/categoria.js
+++ route/categoria.js
@@ -1,28 +1,28 @@
 const express = require('express');
 
 //gerenciador de rotas do express
+const modelCategoria = require('../model/modelCategorias');
 
-const router = express.Router;
+const router = express.Router();
 
-app.post('/cadastrarCategoria', (req, res)=>{
-
-    
+router.post('/cadastrarCategoria', (req, res)=>{
     res.send("ROTA DE CADASTRO DE CATEGORIA!");
     //console.log("TESTE DE NODEMON!");
 });
 
 //ROTA DE LEITURA DE CATEGORIA
-app.get('/listarCategoria', (req, res)=>{
+router.get('/listarCategoria', (req, res)=>{
     res.send('ROTA DE LISTAGEM DE CATEGORIA!');
 });
 
 //ROTA DE ALTERAÇÃO DE CATEGORIA
-app.put('/alterarCategoria', (req, res)=>{
+router.put('/alterarCategoria', (req, res)=>{
     res.send('ROTA DE ALTERAÇÃO DE CATEGORIA!');
 });
 
 //ROTA DE EXCLUSÃO DE CATEGORIA
-app.delete('/excluirCategoria', (req, res)=>{
+router.delete('/excluirCategoria', (req, res)=>{
     res.send('ROTA DE EXCLUSÃO DE CATEGORIA!');
 });
 
+module.exports = router;
