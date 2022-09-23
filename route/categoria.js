07b297c886ed2a2686a64f57034441fbdcee68cc
Talita Marcondes <70537487+talitatodesco@users.noreply.github.com>
2022-09-23 17:50:40 -0300

    Arrumando Codigo

--- route/categoria.js
+++ route/categoria.js
@@ -1,13 +1,17 @@
+/*IMPORTA O PACOTE DO EXPRESS PARA O SCRIPT index.js*/
 const express = require('express');
 
-//gerenciador de rotas do express
 const modelCategoria = require('../model/modelCategorias');
 
+/*GERENCIADOR DE ROTAS DO EXPRESS*/
 const router = express.Router();
 
+/** ROTAS DE CRUD DE CATEGORIA **/
+//ROTA DE CADASTRO DE CATEGORIA
+//NOME(P1, P2, P3, P4){}
 router.post('/cadastrarCategoria', (req, res)=>{
-    res.send("ROTA DE CADASTRO DE CATEGORIA!");
-    //console.log("TESTE DE NODEMON!");
+    res.send('ROTA DE CADASTRO DE CATEGORIA!');
+    // console.log('TESTE DE NODEMON');
 });
 
 //ROTA DE LEITURA DE CATEGORIA
@@ -25,4 +29,4 @@ router.delete('/excluirCategoria', (req, res)=>{
     res.send('ROTA DE EXCLUSÃO DE CATEGORIA!');
 });
 
-module.exports = router;
+module.exports = categoria;
