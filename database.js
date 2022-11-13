98a623c7b038a1282b14642591cb77a07348c7ae
Talita Marcondes <70537487+talitatodesco@users.noreply.github.com>
2022-11-13 10:26:18 -0300

    Add files via upload

---
+++ database.js
@@ -0,0 +1,26 @@
+//IMPORTAÇÃO DO PACOTE SEQUELIZE
+const Sequelize = require('sequelize');
+
+/***** CRIA A CONEXÃO COMO BANCO DE DADOS *****/
+/*
+PARAMETROS DO SEQUELIZE
+1 - NOME DO BANCO - STRING
+2 - USUÁRIO DO BANCO - STRING
+3 - SENHA DO BANCO - STRING
+4 - JSON:
+    4.1 - O LOCAL ONDE O BANCO ESTÁ EXECUTANTO (host)
+    4.2 - TIPO DO BANCO (dialect)
+    4.3 - DEFINE O FUSO HORARIO LOCAL
+*/
+const connection = new Sequelize(
+        'bd_libri_api', 
+        'root',
+        '',
+        {
+            host:'localhost',
+            dialect:'mysql',
+            timezone: '-03:00'
+        }
+    );
+
+module.exports = connection;
